c0c40ec583086447520a53253968c81d0371e1da
ttech5593 <ttech5593@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2007-02-09 00:00:13 +0000

    git-svn-id: https://plugins.svn.wordpress.org/miniblog/trunk@7891 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- miniblog.php
+++ miniblog.php
@@ -4,7 +4,7 @@ Plugin Name: Miniblog
 Plugin URI: http://mediumbagel.org/?page_id=16
 Description: Allows miniature blogs, links, notes, asides, or whatever to be created. The menu, functionality, and documentation can be found in the Write : Miniblog menu once the plugin is activated. This plugin was originally authored by <a href="http://www.nmyworld.com/">Ryan Poe</a>.
 Version: 0.14
-Author: Thomas Cort
+Author: Joe
 Author URI: http://mediumbagel.org/
 */
 
